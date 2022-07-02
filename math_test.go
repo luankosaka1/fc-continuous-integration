d2fe5aa4bf65570e3e37fe82a657112801f2c068
Luan Kosaka <luan.kosaka@webjump.com.br>
2022-07-01 23:10:28 -0300

    feat: change test

--- math_test.go
+++ math_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := soma(15, 15)
+	total := soma(10, 10)
 
-	if total != 30 {
-		t.Errorf("Expected %v but got %v", 30, total)
+	if total != 20 {
+		t.Errorf("Expected %v but got %v", 20, total)
 	}
 }
